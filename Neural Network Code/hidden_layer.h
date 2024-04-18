8eee974d704a95916dca98f6970f3f18b391cf81
alephys26 <119301033+alephys26@users.noreply.github.com>
2024-04-18 19:55:28 +0530

    Updated hidden_layer.h

--- Neural Network Code/hidden_layer.h
+++ Neural Network Code/hidden_layer.h
@@ -8,18 +8,87 @@ Course Project
 Helper code for Layer behaviour
 -------------------------------------
 Currently useful
-Models Layer 
+Models Layer
 -------------------------------------
 */
-#ifndef __LAYER
-#define __LAYER
-#include "neuron.h"
+#ifndef __HIDDENLAYER
+#define __HIDDENLAYER
+using namespace std;
 #include <vector>
-class Layer
+#include<string.h>
+class HiddenLayer
 {
-    private:
-        vector<Neuron> neurons;
-        char* activation_func;
-        double 
+private:
+    char *activation_func;
+    int num_neurons;
+    int output_size;
+    pair<int, int> input_size;
+    vector<vector<double>> normalising_scalar;
+    vector<vector<double>> weights;
+    vector<vector<double>> bias;
+    vector<vector<double>> outputs;
+    vector<vector<double>> inputs;
+
+protected:
+    void set_num_next_layer_neurons(const int num_next_layer_neurons)
+    {
+        output_size = num_next_layer_neurons;
+    }
+
+    void set_num_neurons(const int neurons_number)
+    {
+        num_neurons = neurons_number;
+    }
+
+    void set_activation_function(const char *activation_function)
+    {
+        strcpy(activation_func, activation_function);
+    }
+
+    void set_inputs(const vector<vector<double>> &inp)
+    {
+        inputs = inp;
+        input_size = {inputs.size(), inputs[0].size()};
+    }
+
+    const int get_num_next_layer_neurons()
+    {
+        return output_size;
+    }
+
+    const int get_num_neurons()
+    {
+        return num_neurons;
+    }
+
+    const vector<vector<double>> get_inputs()
+    {
+        return inputs;
+    }
+
+    const char *get_activation_function()
+    {
+        return activation_func;
+    }
+
+    const vector<vector<double>> calculate()
+    {
+        int n = num_neurons;
+        int m = input_size.second;
+        int k = input_size.first;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                double sum = 0.0;
+                for (int l = 0; l < k; ++l)
+                {
+                    sum += weights[i][l] * inputs[l][j];
+                }
+                outputs[i][j] = sum + bias[i][j];
+            }
+        }
+        return outputs;
+    }
 };
 #endif
